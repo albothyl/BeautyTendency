a72112e17fdf5d05b949b633e3e8d40c302dab09
알보칠(전형원) <jjhwqqq@coupang.com>
2015-04-13 21:40:08 +0900

    project setting

---
+++ src/test/groovy/DataBaseConnectionGroovyTest.groovy
@@ -0,0 +1,37 @@
+import com.beauty.tendency.configrations.RepositoryConfigration
+import org.junit.Assert
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+import javax.sql.DataSource
+
+
+/**
+ * Created by on 2015. 4. 13..
+ */
+@ContextConfiguration(classes = [RepositoryConfigration.class])
+class DataBaseConnectionGroovyTest extends Specification {
+    @Autowired
+    DataSource dataSource;
+
+    def "데이터베이스 인설트 테스트"() {
+        setup:
+        def connection = dataSource.getConnection();
+        def preparedStatement = connection.prepareStatement("INSERT INTO toby.USERS VALUES(?,?,?)");
+        preparedStatement.setString(1,"testId");
+        preparedStatement.setString(2,"testName");
+        preparedStatement.setString(3,"testPassword");
+
+        def resultLowCount = preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+
+        expect:
+        Assert.assertEquals(resultLowCount, 1);
+    }
+    def "aa"(){
+        expect: 1==1;
+    }
+}
